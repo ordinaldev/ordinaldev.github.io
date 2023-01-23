eae644134d0de6b48c816ac107d85889abb7305c
foxydigital <121743907+foxydigital@users.noreply.github.com>
2023-01-23 17:54:46 +0700

    Delete api-cuaca directory
    Signed-off-by: foxydigital <121743907+foxydigital@users.noreply.github.com>

--- api-cuaca/src/routes/weather.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const controller = require('../controllers/weather');
-
-router.get('/:province', controller.getByProvince);
-router.get('/:province/:city', controller.getByCity);
-
-module.exports = router;
--- api-cuaca/src/utils/responseCreator.js
+++
@@ -1,9 +0,0 @@
-const responseCreator = ({ data = null, message = null }) => {
-  return {
-    success: data ? true : false,
-    message,
-    data,
-  };
-};
-
-module.exports = responseCreator;
